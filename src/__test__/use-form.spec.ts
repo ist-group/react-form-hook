e44df611875b38129da3b7ebcf22cb4851a9115d
Stefan Jonasson <Stefan.Jonasson@ist.com>
2019-11-27 07:50:32 +0100

    Fixed problem with array fields after remove in the mittle of the list.

--- src/__test__/use-form.spec.ts
+++ src/__test__/use-form.spec.ts
@@ -22,10 +22,11 @@ const getBasicFormSetup = (myMock?: jest.Mock<any, any>) =>
         name: "",
         description: "",
         list: [
-          { id: "", name: "", description: "" },
-          { id: "2", name: "Test 2", description: "" },
+          { id: "", name: "", description: "", date: new Date() },
+          { id: "2", name: "Test 2", description: "", date: new Date() },
         ],
-        item: { id: "", name: "", description: "" },
+        item: { id: "", name: "", description: "", date: new Date() },
+        date: new Date(),
       },
       {
         validation: {
@@ -99,6 +100,7 @@ test("Arrays - push", () => {
   expect(result.current.value.list.value[2].value.id.value).toBe("id-test");
   expect(result.current.value.list.value[2].value.name.value).toBe("name-test");
   expect(result.current.value.list.value[2].value.description!.value).toBe("description-test");
+  expect(result.current.value.list.value[2].value.date!.value.toISOString()).toBe(date.toISOString());
 });
 
 test("Arrays -  onChange validation", () => {
@@ -106,14 +108,14 @@ test("Arrays -  onChange validation", () => {
 
   const date = new Date();
   act(() => {
-    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test" });
-    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test" });
-    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test" });
-    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test" });
-    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test" });
-    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test" });
-    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test" });
-    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test" });
+    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test", date });
+    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test", date });
+    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test", date });
+    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test", date });
+    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test", date });
+    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test", date });
+    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test", date });
+    result.current.value.list.push({ id: "id-test", name: "name-test", description: "description-test", date });
   });
   expect(result.current.value.list.error).toBeFalsy();
   act(() => {
@@ -222,13 +224,21 @@ test("Arrays - remove set bindings", () => {
   const { result } = getBasicFormSetup();
 
   act(() => {
-    const setMethod = result.current.value.list.value[1].value.name.set;
-    setMethod("Before remove");
-    result.current.value.list.remove(0);
+    result.current.value.list.push({
+      id: "id-test",
+      name: "name-test",
+      description: "description-test",
+      date: new Date(),
+    });
+    result.current.value.list.remove(1);
     result.current.value.list.value[0].value.name.set("After remove");
   });
   expect(result.current.value.list.value[0].value.name.value).toBe("After remove");
+  expect(result.current.value.list.value[1].value.name.value).toBe("name-test");
   expect(result.current.value.list.value[0].path).toMatchObject(["list", 0]);
+  expect(result.current.value.list.value[1].path).toMatchObject(["list", 1]);
+
+  expect(result.current.value.list.value.length).toBe(2);
 });
 
 test("Date - date to array and back", () => {
